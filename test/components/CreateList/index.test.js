d077891f3b73c1503eabf5041dfa0112b143d501
Mark Muskardin <mark.muskardin@gmail.com>
2016-09-08 20:45:22 -0700

    renamed tests

--- test/components/CreateList/index.test.js
+++ test/components/CreateList/index.test.js
@@ -1,18 +1,18 @@
 import React              from 'react';
 import { mount, shallow } from 'enzyme';
 import { expect }         from 'chai';
-import YoutubePlaylist    from '../../../src/index';
+import CreateList    from '../../../src/CreateList';
 
-describe('<YoutubePlaylist />', function() {
+describe('<CreateList />', function() {
 
   it('should display the right placeholder if set', function() {
-    const component = mount(<YoutubePlaylist
+    const component = mount(<CreateList
                               placeholder='Whatever I want' />);
     expect(component.find('.placeholder').text()).to.equal('Whatever I want');
   });
 
   it('should display a default placeholder', function() {
-    const component = mount(<YoutubePlaylist />);
+    const component = mount(<CreateList />);
     expect(component.find('.placeholder').text()).to.equal('Add to List');
   });
 
